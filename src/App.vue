da6b6c717530efa06dfc67cb8b1e3ee0265c8bb6
Sebastian Boolean <truefalse@posteo.de>
2018-10-09 17:00:39 +0200

    setup page

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,64 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav>
+      <div class="left">
+        <span>RED CABINET</span>
+      </div>
+      <div class="right">
+        <router-link to="/concerts">CONCERTS</router-link>
+        <router-link to="/cabinet">CABINET</router-link>
+      </div>
+    </nav>
     <router-view/>
+    <footer>
+      <router-link to="/about">ABOUT</router-link>
+      <router-link to="/imprint">IMPRINT</router-link>
+    </footer>
   </div>
 </template>
 
 <style lang="scss">
+$primary-color: rgb(187, 40, 48);
+* {
+  box-sizing: border-box;
+}
+html,
+body {
+  height: 100%;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  font-weight: bold;
   text-align: center;
-  color: #2c3e50;
+  color: black;
+  height: 100%;
+  display: flex;
+  padding: 20px;
+  flex-direction: column;
+  justify-content: space-between;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+a {
+  color: black;
+  text-decoration: none;
+  transition: color 300ms ease-out;
+  &:hover,
+  &:active {
+    color: $primary-color;
   }
+  &:first-child {
+    margin-right: 30px;
+  }
+}
+nav {
+  display: flex;
+  justify-content: space-between;
+  font-size: 30px;
+}
+footer {
 }
 </style>
