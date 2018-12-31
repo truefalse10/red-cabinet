8130a47a35b5e9e6de4068a5f8e50037d9d61404
Sebastian Boolean <truefalse@posteo.de>
2018-12-31 16:49:54 +0100

    change rewrite rule

--- public/functions.php
+++ public/functions.php
@@ -4,7 +4,7 @@ remove_action('template_redirect', 'redirect_canonical');
 
 // Redirect all requests to index.php so the Vue app is loaded and 404s aren't thrown
 function remove_redirects() {
-    add_rewrite_rule('^/(?!img\/)(.+)/?', 'index.php', 'top');
+    add_rewrite_rule('^(?!img\/)/(.+)/?', 'index.php', 'top');
 }
 add_action('init', 'remove_redirects');
 
