900fc902b987f16467ae532b6cbd9c50d9760938
Sebastian Boolean <15369187+truefalse10@users.noreply.github.com>
2019-06-20 21:37:18 +0200

    change date

--- src/views/Cabinet.vue
+++ src/views/Cabinet.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="cabinet">
-    <p class="next">Next Cabinet:<br>15.03.19</p>
+    <p class="next">Next Cabinet:<br>23.06.19</p>
     <vue-markdown :source="file"/>
     <p class="small">Buy tickets or subscribe for further<br>informations and entry</p>
     <div class="buttons">
